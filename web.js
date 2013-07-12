66c2ce7d65189fbfd09def58e7bc6883a5a6f5fb
shockguy <sean.k.treadway@gmail.com>
2013-07-12 01:20:35 +0000

    First attempt at pulling from index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var contents = fs.readFileSync(index.html);
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(contents.toString());
 });
 
 var port = process.env.PORT || 5000;
