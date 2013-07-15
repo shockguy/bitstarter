b749e0b6fc0387b75a989fce4497d4a83aaf671d
shockguy <sean.k.treadway@gmail.com>
2013-07-15 00:13:17 +0000

    Updated web.js to use say variable

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var contents = fs.readFileSync('index.html');
 var say = contents.toString();
-  response.send('This is a check 0712');
+  response.send(say);
 });
 
 var port = process.env.PORT || 5000;
